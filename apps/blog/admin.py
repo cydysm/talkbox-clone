1fc510ee9f798f5836a9b00f55a682da83c17d98
cydysm <t.ccydlj@gmail.com>
2026-08-26 11:50:13 +0800

    chore: remove unused admin imports

--- apps/blog/admin.py
+++ apps/blog/admin.py
@@ -1,6 +1,4 @@
 from django.contrib import admin
-from django.contrib.admin import AdminSite
-from django.utils.translation import gettext_lazy as _
 
 from .models import Category, PluginSetting, Post, ThemeSetting
 
